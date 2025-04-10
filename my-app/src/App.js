7b0654fc5445ef2ef1d002da65b4c4584e25abb8
Emirq00 <emirquezadao@gmail.com>
2025-04-09 18:59:47 -0600

    ft(): Adds the main page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import CountdownTimer from "./CountDownTimer";
 import './App.css';
 
 function App() {
+
+  const targetDate = "2025-06-28T19:30:00";
+
+  const images=[
+    "/ImagenesInvitacion/IMG_1908.jpg",
+    "/ImagenesInvitacion/IMG_2474.jpg",
+    "/ImagenesInvitacion/IMG_2512.jpg"
+  ];
+
+
   return (
     <div className="App">
-      <div className='imperial-script-regular' style={{fontSize:'20vh', color:'006527'}}>
+      <div className='imperial-script-regular' style={{fontSize:'5vh', color:'#006527', paddingTop:'7vh', fontWeight:'bold', textAlign:'left', paddingLeft:'4vw'}}>
         Mis XV años
       </div>
+      <div className='imperial-script-regular' style={{fontSize:'5vh', color:'#006527', fontWeight:'bold', textAlign:'center', paddingLeft:'3vw', float:'left', width:'25vh'}}>
+        <p>Salma Quezada Olivares</p>
+      </div>
+      <div className='rectangulo-rotado' style={{paddingLeft:'10vw'}}>
+        <img src='/ImagenesInvitacion/CarreteCamara.png' alt='Camera reel' className='rectangulo-rotado' style={{rotate:'-20deg', right:'15vw', width:'75vw', height:'75vh'}}/>
+        {images.map((img, index) =>(
+          <img 
+            key={index}
+            src={img}
+            alt='Sliding'
+            className='sliding-image'
+            style={{'--i':index}}            
+            />
+        ))}
+      </div>
+      <CountdownTimer targetDate={targetDate}/>
+      <img src='/ImagenesInvitacion/MariposaVerde.png' alt='Butterfly' className='image butterfly-1' style={{bottom: '131vh', left: '50vw', width:'15vw'}}/>
+      <img src='/ImagenesInvitacion/MariposaVerde.png' alt='Butterfly' className='image butterfly-1' style={{bottom:'100vh', left:'80vw', width:'20vw'}}/>
+      <img src='/ImagenesInvitacion/VariasMariposasVerdes.png' alt='Butterfly' className='image butterfly-2' style={{bottom:'115vh', left:'43vw'}}/>
+      <img src='/ImagenesInvitacion/MariposaVerde.png' alt='Butterfly' className='image butterfly-1' style={{bottom:'90vh', left:'20vw', width:'20vw'}}/>
+      <img src='/ImagenesInvitacion/FloresVerdes.png' alt='GreenFlower' className='image flower' style={{bottom:'80vh', left:'65vw'}}/>
+      <img src='/ImagenesInvitacion/MasFloresVerdes.png' alt='Flowers' className='image flower' style={{bottom:'135vh', left:'70vw', rotate:'90deg'}}/>
     </div>
+    
+
   );
 }
 
---
+++ my-app/src/CountDownTimer.js
@@ -0,0 +1,53 @@
+import { useState, useEffect, useCallback } from "react"; 
+import './App.css';
+
+
+const CountDownTimer = ({targetDate}) =>{
+    const calculateTimeLeft = useCallback(() => {
+        const difference = +new Date(targetDate)-+new Date();
+        let timeLeft = {};
+        if(difference>0){
+            timeLeft={
+                days:Math.floor(difference/(1000*60*60*24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+        return timeLeft;
+    }, [targetDate]);
+    const [timeLeft, setTimeLeft]=useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setInterval(() =>{ 
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [calculateTimeLeft]);
+
+    const padNumber = (num) => num.toString().padStart(2, "0");
+
+    return(
+        <div className="countdown-container">
+            <div className="time-box">
+                <span className="imperial-script-regular">Solo faltan:</span>
+                <br/>
+                <span className="imperial-script-regular" style={{fontSize:'8vw'}}>{padNumber(timeLeft.days || 0)}</span>
+                <span className="imperial-script-regular" style={{fontSize:'8vw'}}> : {padNumber(timeLeft.hours || 0)}</span>
+                <span className="imperial-script-regular" style={{fontSize:'8vw'}}> : {padNumber(timeLeft.minutes || 0)}</span>
+                <span className="imperial-script-regular" style={{fontSize:'8vw'}}> : {padNumber(timeLeft.seconds || 0)}</span>
+            </div>
+            <div className="time-box">
+                <span className="imperial-script-regular" style={{fontSize:'4vw', padding:'2vw'}}>Días </span>
+                <span className="imperial-script-regular" style={{fontSize:'4vw', padding:'1vw'}}>Horas </span>
+                <span className="imperial-script-regular"style={{fontSize:'4vw', padding:'1vw'}}>Minutos </span>
+                <span className="imperial-script-regular"style={{fontSize:'4vw'}}>Segundos</span>
+            </div>
+            
+        </div>
+    )
+    
+}
+
+export default CountDownTimer;
+
