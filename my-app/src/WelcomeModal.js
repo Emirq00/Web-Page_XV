896c98a90cd571664afe94c0090f9667b37cfbd4
Emirq00 <emirquezadao@gmail.com>
2025-04-15 18:37:21 -0600

    ft(): Fixes again the welcomeModal

--- my-app/src/WelcomeModal.js
+++ my-app/src/WelcomeModal.js
@@ -17,19 +17,10 @@ const WelcomeModal = ({ onClose }) => {
         exit={{ scale: 0.8, opacity: 0 }}
         transition={{ type: 'spring', stiffness: 100 }}
       >
-        {/* <img src='/ImagenesInvitacion/Sobre.png' alt='Letter envelope' className='image envelope'/> */}
-        {/* <h2 style={{ fontSize: '2.5rem', marginBottom: '1rem'}}>¡Bienvenido a mis XV!</h2>
-        <p style={{ fontSize: '1.2rem', marginBottom: '2rem' }}>
-          Estás por entrar a la experiencia digital de mi fiesta
-        </p> */}
-        
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="enter-button"
-          onClick={onClose}
-        >
-        </motion.button> 
+        <button className='enter-button' onClick={onClose}>
+
+        </button>
+
       </motion.div>
     </div>
   );
